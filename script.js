85d6042a4d6814e880406a47ffab409c8348e052
Pasztor Adam <adam.psztr@icloud.com>
2023-06-19 00:51:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.querySelector(".navbar-toggler").addEventListener("click", ()=>{
-	document.querySelector(".navbar").classList.toggle("linearGreen");
+	document.querySelector(".navbar").classList.toggle("linearGreenR");
 	document.querySelector(".navbar").classList.toggle("linearGreenMove");
 })
